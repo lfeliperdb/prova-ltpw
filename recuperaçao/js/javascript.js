fe23a68e1c0431627031e16fe30ff349b2e06aa6
Luis Felipe <rodrigo.santos.estudante@eteot.rio.br>
2022-05-10 11:47:46 -0300

    !!

--- recuperaçao/js/javascript.js
+++ recuperaçao/js/javascript.js
@@ -4,23 +4,47 @@ function calcpedido() {
     bb = document.getElementById("bebida").value;
     btt = document.getElementById("batata").value;
 
+
     qs = document.getElementById("quantSand").value;
     qb = document.getElementById("quantBeb").value;
     qbt = document.getElementById("quantBat").value;
 
-
     //processamento  
+
+    //hamburguer
     if (sd == "bigmac") {
-        valorsd = 14,90
+        valorsd = 14.90;
+        imagem = "img/bigmac.jpg"
     } else if (sd == "mcfish") {
-        valorsd = 12,90 
+        valorsd = 12.90;
+        imagem = "img/mcfish.jpg"
     } else if (sd == "qtr") {
-        valorsd = 13,90
-    }    
+        imagem = "img/quarterao.jpg"
+        valorsd = 13.90;
+    }
+
+    //bebida
+    if (bb == "p") {
+        valorbb = 5.90;
+    } else if (bb == "m") {
+        valorbb = 6.90;
+    } else if (bb = "g") {
+        valorbb = 7.90;
+    }
+
+    //batata
+    if (btt == "p") {
+        valorbtt = 5.90;
+    } else if (btt == "m") {
+        valorbtt = 6.90;
+    } else if (btt = "g") {
+        valorbtt = 7.90;
+    }
 
 
-    total = (valorsd*qs) 
+    total = (valorsd*qs) + (valorbb*qb) + (valorbtt*qbt);
 
     //saida
-    document.getElementById("total") = total.toFixed(2);
+    document.getElementById("total").value = total.toFixed(2);
+    document.getElementById("foto").src = imagem;
 }
